a1c647829e5232430c273c42127cb2a083faae6e
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-07-24 13:22:48 +0700

    deploy vercel

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('hello world')
+  res.send('hello world')
 })
 
 const logicTest = require('./logic-test.route')
