a1a353ba497049bcc1d1312ec2017618b7978a52
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-07-25 04:24:57 +0700

    response image micro service

--- src/app.js
+++ src/app.js
@@ -5,6 +5,13 @@ app.set('json spaces', 4);
 
 const routes = require('./routes/index.route');
 app.use(routes)
-app.use('/assets/img/', express.static(process.cwd() + '/src/assets/img/'))
+
+const path = require('path');
+const fs = require("fs");
+app.use('/assets/img/microservice.png', (req, res) => {
+  var img = fs.readFileSync(path.join(process.cwd()+'/src/assets/img/microservice.png'));
+  res.writeHead(200, {'Content-Type': 'image/png' });
+  res.end(img, 'binary');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
