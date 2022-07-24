b06aefb5d909de243c6ab3342a69bce656712465
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-07-24 16:29:42 +0700

    test render html file in vercel

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router();
+const path = require('path');
 
 router.get('/', (req, res) => {
   res.send('hello world')
+})
+
+router.get('/coffee-cup', (req, res) => {
+  res.sendFile(path.join(process.cwd()+'/src/view/coffee-cup.html'));
 })
 
 const logicTest = require('./logic-test.route')
