e87e303df654b5720b3200b88d9ba2d91dd0fcfe
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-07-24 17:27:40 +0700

    add res json space formatting

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8081
+app.set('json spaces', 4);
 
 const routes = require('./routes/index.route');
 app.use(routes)
