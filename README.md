b98b4b8a05195fc9603cf7235278b4896f0aa089
Prasit Kaew. Champathong <prasit.c@cjworx.com>
2022-07-25 05:12:25 +0700

    add refactor code

--- README.md
+++ README.md
@@ -14,6 +14,9 @@ npm start
 - [แบบระบุตัวแปร](https://nodejs-test-one.vercel.app/logic-test/?name=Somchai&surname=Thomson&gender=M&birthDate=1/1/1990)
 - [code logic-test.controller.js](https://github.com/tempKaew/nodejs-test/blob/main/src/controllers/logic-test.controller.js)
 
+## Refactor code
+- [code refactore.controller.js](https://github.com/tempKaew/nodejs-test/blob/main/src/controllers/refactore.controller.js)
+
 ## Coffee cup
 - [coffee-cup](https://nodejs-test-one.vercel.app/coffee-cup)
 - [code coffee-cup.html](https://github.com/tempKaew/nodejs-test/blob/main/src/view/coffee-cup.html)
--- src/controllers/refactore.controller.js
+++ src/controllers/refactore.controller.js
@@ -1,14 +1,40 @@
 const axios = require('axios').default;
 
+//ไม่ต้องทำการ Refactore function นี้
+const User = () => {
+  return {
+    insert: function (data) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({ message: "saved", data: data });
+        }, 1000);
+      });
+    },
+  };
+}
+
 const getToDoList = async(req, res) => {
-  try {
-    const re = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    console.log('response: ', re.data);
-    res.json(re.data);
-  } catch (error) {
-    console.error(error)
-    res.json({'status' : 'error'})
-  }
+  var tasksId = [1, 3, 5, 7, 9, 11, 13];
+  var togoList = [];
+  const loopTask = tasksId.map(async(id) => {
+    await axios.get('https://jsonplaceholder.typicode.com/todos/'+id)
+    .then(async(res) => {
+      const insert = await User().insert({
+        id: res.data.id,
+        userId: res.data.userId,
+        title: res.data.title,
+        remark: "Title " + res.data.title + "Write by " + res.data.userId,
+        status: res.data.completed === true ? 'Complete' : 'Not complete',
+      })
+      // console.log('success id :',res.data.id)
+      togoList.push(insert);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  });
+  await Promise.all(loopTask);
+  res.json({'togoList' : togoList})
 }
 
 module.exports = {
